b4221991a4afc0b61c7c09344e78bd1b0bc75508
babtsoualiaksandr <babtsoualiaksandr@gmail.com>
2019-11-21 15:46:35 +0300

    fix: bag

--- index.js
+++ index.js
@@ -1,65 +1,49 @@
-function draw4x4() {
-    var dataJson = [];
-    fetch('./assets/data/4x4.json')
-        .then(res => res.json())
-        .then((data) => {
-            console.log('data', data);
-            const canvas = document.querySelector("#canvas");
-            const ctx = canvas.getContext('2d');
-            const width = document.querySelector("#canvas").offsetWidth / 4;
-            const height = document.querySelector("#canvas").offsetHeight / 4;
-            data.forEach((line, i) => {
-                console.log(line, i);
-                line.forEach((cell, j) => {
-                    ctx.fillStyle = '#' + cell;
-                    ctx.font = "12px serif";
-                    ctx.fillRect(width * i, height * j, width, height);
-                });
-            });
-        });
-}
-
-document.querySelector('.btn4x4').addEventListener('click', draw4x4);
-
-function draw32x32() {
-    var dataJson = [];
-    fetch('./assets/data/32x32.json')
-        .then(res => res.json())
-        .then((data) => {
-            console.log('data', data);
-            const canvas = document.querySelector("#canvas");
-            const ctx = canvas.getContext('2d');
-            const width = document.querySelector("#canvas").offsetWidth / 32;
-            const height = document.querySelector("#canvas").offsetHeight / 32;
-            data.forEach((line, i) => {
-                console.log(line, i);
-                line.forEach((cell, j) => {
-                    console.log(cell, i, j, width, height);
-                    ctx.fillStyle = 'rgba(' + cell[0] + ',' + cell[1] + ',' + cell[2] + ',' + cell[3] / 255 + ')';
-                    ctx.font = "12px serif";
-                    ctx.fillRect(width * i, height * j, width, height);;
-                });
-            });
-        });
-}
-
-
-document.querySelector('.btn32x32').addEventListener('click', draw32x32);
-
-function drawLogo() {
+function drawPix(url, pixel) {
     const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext('2d')
-    console.log(ctx)
-    var img = new Image();
-    console.log(img);
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0, 512, 512);
-    };
-    img.src = './assets/data/RSS_logo.png';
+    const ctx = canvas.getContext('2d');
+    if (pixel === 0) {
+        const img = new Image();
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0, 512, 512);
+        };
+        img.src = url;
+    } else {
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+                const canvas = document.querySelector("#canvas");
+                const ctx = canvas.getContext('2d');
+                if (pixel > 0) {
+                    const width = document.querySelector("#canvas").offsetWidth / pixel;
+                    const height = document.querySelector("#canvas").offsetHeight / pixel;
+                    data.forEach((line, i) => {
+                        line.forEach((cell, j) => {
+                            if (pixel === 4) ctx.fillStyle = '#' + cell;
+                            if (pixel === 32) ctx.fillStyle = 'rgba(' + cell[0] + ',' + cell[1] + ',' + cell[2] + ',' + cell[3] / 255 + ')';
+                            ctx.fillRect(width * i, height * j, width, height);
+                        });
+                    });
+                }
+            });
+    }
 }
-
-document.querySelector('.btnLogo').addEventListener('click', drawLogo);
-
+function clickBtn(event) {
+    const btnClick = event.target.classList[1];
+    switch (btnClick) {
+        case 'btn4x4':
+            drawPix('./assets/data/4x4.json', 4);
+            break;
+        case 'btn32x32':
+            drawPix('./assets/data/32x32.json', 32);
+            break;
+        case 'btnLogo':
+            drawPix('./assets/data/RSS_logo.png', 0);
+            break;
+        default:
+            break;
+    }
+}
+document.querySelector('.btns').addEventListener('click', clickBtn);
 
 
 
